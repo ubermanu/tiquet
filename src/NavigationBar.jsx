f1cbedf37f1ffbe0fb89e0117365aa6b84a7d084
ubermanu <e.vodor@gmail.com>
2020-11-29 09:21:16 +0100

    Update navigation with correct mentions

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -5,36 +5,39 @@ function NavigationBar() {
     <nav className="navbar">
       <div className="navbar-content">
         <button className="btn btn-action" type="button">
-          <i className="fa fa-bars" aria-hidden="true"></i>
+          <i className="fa fa-bars" aria-hidden="true"/>
           <span className="sr-only">Toggle sidebar</span>
         </button>
       </div>
       <a href="#" className="navbar-brand">
         <span>🦟</span>
-        Tiquet
+        <span>Tiquet</span>
       </a>
-      <span
-        className="navbar-text text-monospace">v3.0</span>
+      <span className="navbar-text text-monospace">v0.1</span>
       <ul
         className="navbar-nav d-none d-md-flex">
         <li className="nav-item active">
-          <a href="#" className="nav-link">Docs</a>
+          <a href="#" className="nav-link">Projects</a>
         </li>
         <li className="nav-item">
-          <a href="#" className="nav-link">Products</a>
+          <a href="#" className="nav-link">Issues</a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">Settings</a>
         </li>
       </ul>
-      <form className="form-inline d-none d-md-flex ml-auto" action="..."
-            method="...">
-        <input type="text" className="form-control" placeholder="Email address" required="required" />
-        <button className="btn btn-primary" type="submit">Sign up</button>
+      <form className="form-inline d-none d-md-flex ml-auto" action="">
+        <input type="text" className="form-control" placeholder="Search anything..." required={true} />
+        <button className="btn btn-primary" type="submit">
+          <i className="fa fa-search" aria-hidden="true"/>
+        </button>
       </form>
       <div
         className="navbar-content d-md-none ml-auto">
         <div className="dropdown with-arrow">
           <button className="btn" data-toggle="dropdown" type="button" id="navbar-dropdown-toggle-btn-1">
             Menu
-            <i className="fa fa-angle-down" aria-hidden="true"></i>
+            <i className="fa fa-angle-down" aria-hidden="true"/>
           </button>
           <div className="dropdown-menu dropdown-menu-right w-200"
                aria-labelledby="navbar-dropdown-toggle-btn-1">
