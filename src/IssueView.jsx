ae1a77703a18daf5258c0e2b4110d6b3c7400da7
ubermanu <e.vodor@gmail.com>
2020-11-29 09:06:08 +0100

    Add issue view

---
+++ src/IssueView.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function IssueView() {
+  return (
+    <section className="container">
+      <article className="content">
+        <h2 className="content-title">
+          It was going to rain
+        </h2>
+        <p>
+          The weather forecast didn't say that, but the steel plate in his hip did. He had learned over the years to
+          trust his hip over the weatherman. It was going to rain, so he better get outside and prepare. Sleeping in his
+          car was never the plan but sometimes things don't work out as planned. This had been his life for the last
+          three months and he was just beginning to get used to it. He didn't actually enjoy it, but he had accepted it
+          and come to terms with it. Or at least he thought he had. All that changed when he put the key into the
+          ignition, turned it and the engine didn't make a sound.
+        </p>
+      </article>
+    </section>
+  )
+}
+
+export default IssueView
