c7ee0cd06d09d2953f312b77cfd730f5c81a1c65
ubermanu <e.vodor@gmail.com>
2020-11-29 14:43:40 +0100

    Move exclude logic into its own filter

--- src/reducers/issueReducer.js
+++ src/reducers/issueReducer.js
@@ -1,15 +1,19 @@
 const initialState = []
 
+function exclude(issue) {
+  return ({ id }) => id !== issue.id
+}
+
 export default function issueReducer(state = initialState, action) {
   switch (action.type) {
     case 'ADD_ISSUE': {
       return [...state, action.payload]
     }
     case 'UPDATE_ISSUE': {
-      return [...state.filter(({ id }) => id !== action.payload.id), action.payload]
+      return [...state.filter(exclude(action.payload)), action.payload]
     }
     case 'DELETE_ISSUE': {
-      return [...state.filter(({ id }) => id !== action.payload.id)]
+      return [...state.filter(exclude(action.payload))]
     }
   }
   return state
