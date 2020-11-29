46412aef572f9bede3b980cda9327d6c06fe18d8
ubermanu <e.vodor@gmail.com>
2020-11-29 08:48:30 +0100

    Remove old css and show issue list view

---
+++ src/IssueListView.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function IssueListView() {
+
+  const title = ''
+  const assignedTo = []
+  const body = ''
+
+  return (
+    <article className="container">
+
+    </article>
+  )
+}
+
+export default IssueListView
