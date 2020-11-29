28988f313dce55c0cb034b0cc838c45518eb67eb
ubermanu <e.vodor@gmail.com>
2020-11-29 09:38:18 +0100

    Add basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
 import React from 'react'
 import 'halfmoon/css/halfmoon-variables.min.css'
 import './App.css'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
 import NavigationBar from './NavigationBar'
 import IssueListView from './IssueListView'
 import ProjectListView from './ProjectListView'
 
 function App() {
   return (
-    <main className="with-custom-webkit-scrollbars with-custom-css-scrollbars"
-          data-dm-shortcut-enabled="true"
-          data-sidebar-shortcut-enabled="true"
-          data-set-preferred-mode-onload="true">
-      <div className="page-wrapper with-navbar with-sidebar">
-        <div className="sticky-alerts" />
-        <NavigationBar />
-        <div className="sidebar" />
-        <div className="content-wrapper">
-          <ProjectListView />
+    <Router>
+      <main className="with-custom-webkit-scrollbars with-custom-css-scrollbars"
+            data-dm-shortcut-enabled="true"
+            data-sidebar-shortcut-enabled="true"
+            data-set-preferred-mode-onload="true">
+        <div className="page-wrapper with-navbar with-sidebar">
+          <div className="sticky-alerts" />
+          <NavigationBar />
+          <div className="sidebar" />
+          <div className="content-wrapper">
+            <Switch>
+              <Route path="/projects">
+                <ProjectListView />
+              </Route>
+              <Route path="/issues">
+                <IssueListView />
+              </Route>
+              <Route path="/">
+                <IssueListView />
+              </Route>
+            </Switch>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </Router>
   )
 }
 
--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+
+import { Link } from 'react-router-dom'
+
 function NavigationBar() {
   return (
     <nav className="navbar">
       <div className="navbar-content">
         <button className="btn btn-action" type="button">
-          <i className="fa fa-bars" aria-hidden="true"/>
+          <i className="fa fa-bars" aria-hidden="true" />
           <span className="sr-only">Toggle sidebar</span>
         </button>
       </div>
@@ -17,19 +20,19 @@ function NavigationBar() {
       <ul
         className="navbar-nav d-none d-md-flex">
         <li className="nav-item active">
-          <a href="#" className="nav-link">Projects</a>
+          <Link to="/projects" className="nav-link">Projects</Link>
         </li>
         <li className="nav-item">
-          <a href="#" className="nav-link">Issues</a>
+          <Link to="/issues" className="nav-link">Issues</Link>
         </li>
         <li className="nav-item">
-          <a href="#" className="nav-link">Settings</a>
+          <Link to="/settings" className="nav-link">Settings</Link>
         </li>
       </ul>
       <form className="form-inline d-none d-md-flex ml-auto" action="">
         <input type="text" className="form-control" placeholder="Search anything..." required={true} />
         <button className="btn btn-primary" type="submit">
-          <i className="fa fa-search" aria-hidden="true"/>
+          <i className="fa fa-search" aria-hidden="true" />
         </button>
       </form>
       <div
@@ -37,7 +40,7 @@ function NavigationBar() {
         <div className="dropdown with-arrow">
           <button className="btn" data-toggle="dropdown" type="button" id="navbar-dropdown-toggle-btn-1">
             Menu
-            <i className="fa fa-angle-down" aria-hidden="true"/>
+            <i className="fa fa-angle-down" aria-hidden="true" />
           </button>
           <div className="dropdown-menu dropdown-menu-right w-200"
                aria-labelledby="navbar-dropdown-toggle-btn-1">
