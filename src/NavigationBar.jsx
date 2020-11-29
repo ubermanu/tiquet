b7b98ed7b0acf1b3d74610d20a3f24146092dea7
ubermanu <e.vodor@gmail.com>
2020-11-29 10:25:32 +0100

    Toggle menu

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import classNames from 'classnames'
 
 function NavigationBar() {
+  const [open, setOpen] = useState(false)
+
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <span>🦟</span>
         </Link>
-        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
-           data-target="navbarBasicExample">
+        <a role="button" aria-label="menu" aria-expanded="false" onClick={() => setOpen(!open)}
+           className={classNames('navbar-burger', 'burger', open && 'is-active')}>
           <span aria-hidden="true" />
           <span aria-hidden="true" />
           <span aria-hidden="true" />
         </a>
       </div>
-      <div id="navbarBasicExample" className="navbar-menu">
+      <div className={classNames('navbar-menu', open && 'is-active')}>
         <div className="navbar-start">
           <Link to="/" className="navbar-item">Home</Link>
           <Link to="/issues" className="navbar-item">Issues</Link>
