3b269c1915b7551f22afa4c12757127ad7a0405a
ubermanu <e.vodor@gmail.com>
2020-11-29 14:18:34 +0100

    Update detail view with real data

--- src/IssueView.jsx
+++ src/IssueView.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import NotFoundView from './NotFoundView'
 
 function IssueView() {
   const { id } = useParams()
+  const issue = useSelector(state => state.issues.filter(issue => issue.id === id)[0])
 
   return (
-    <div className="container">
-      <article className="content">
-        <h2 className="content-title">
-          It was going to rain
-        </h2>
-        <p>
-          The weather forecast didn't say that, but the steel plate in his hip did. He had learned over the years to
-          trust his hip over the weatherman. It was going to rain, so he better get outside and prepare. Sleeping in his
-          car was never the plan but sometimes things don't work out as planned. This had been his life for the last
-          three months and he was just beginning to get used to it. He didn't actually enjoy it, but he had accepted it
-          and come to terms with it. Or at least he thought he had. All that changed when he put the key into the
-          ignition, turned it and the engine didn't make a sound.
-        </p>
-      </article>
-    </div>
+    <>
+      {issue ? (
+        <div className="container">
+          <article className="content">
+            <h2 className="content-title">{issue.title}</h2>
+            <p>{issue.description}</p>
+          </article>
+        </div>
+      ) : (
+        <NotFoundView />
+      )}
+    </>
   )
 }
 
