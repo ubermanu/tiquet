6df3de304469ccdfcb4f4fd34a8f849ac528bbaa
ubermanu <e.vodor@gmail.com>
2020-12-17 17:14:06 +0000

    Implement base router

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,14 @@
 <script>
+  import Router from 'svelte-spa-router'
+  import routes from './routes'
   import NavigationBar from './NavigationBar.svelte'
-
-  export let url = ''
-
-  // import IssueListPage from './IssueListPage'
-  // import IssueDetailPage from './IssueDetailPage'
-  // import IssueCreatePage from './IssueCreatePage'
-  // import NotFoundPage from './NotFoundPage'
-  // import IssueEditPage from './IssueEditPage'
-  // import ToastRenderer from './ToastRenderer'
-  // import SearchResultPage from './SearchResultPage'
 </script>
 
 <main>
   <NavigationBar />
   <!--    <ToastRenderer />-->
   <section class="section">
-    <!--        <Route exact path="/issue/:id">-->
-    <!--          <IssueDetailPage />-->
-    <!--        </Route>-->
-    <!--        <Route exact path="/issue/:id/edit">-->
-    <!--          <IssueEditPage />-->
-    <!--        </Route>-->
-    <!--        <Route exact path="/issues/new">-->
-    <!--          <IssueCreatePage />-->
-    <!--        </Route>-->
-<!--    <Route exact path="/issues">-->
-      <!--        <IssueListPage />-->
-<!--    </Route>-->
-    <!--      <Route exact path="/search">-->
-    <!--        <SearchResultPage />-->
-    <!--      </Route>-->
-    <!--      <Route exact path="/">-->
-    <!--        <Redirect to="/issues" />-->
-    <!--      </Route>-->
-    <!--      <Route path="*">-->
-    <!--        <NotFoundPage />-->
-    <!--      </Route>-->
+    <Router {routes} />
   </section>
   <footer class="footer">
     <div class="content has-text-centered">
