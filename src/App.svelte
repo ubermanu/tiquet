d757b137f3c65d9462584806fe6601a016649bbd
ubermanu <e.vodor@gmail.com>
2020-12-17 16:39:52 +0000

    Replace router in app

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { Router, Route } from 'svelte-routing'
+  // import NavigationBar from './NavigationBar'
+
+  export let url = ''
+
+  // import IssueListPage from './IssueListPage'
+  // import IssueDetailPage from './IssueDetailPage'
+  // import IssueCreatePage from './IssueCreatePage'
+  // import NotFoundPage from './NotFoundPage'
+  // import IssueEditPage from './IssueEditPage'
+  // import ToastRenderer from './ToastRenderer'
+  // import SearchResultPage from './SearchResultPage'
+</script>
+
+<Router {url}>
+  <main>
+    <!--    <NavigationBar />-->
+    <!--    <ToastRenderer />-->
+    <section class="section">
+      <!--        <Route exact path="/issue/:id">-->
+      <!--          <IssueDetailPage />-->
+      <!--        </Route>-->
+      <!--        <Route exact path="/issue/:id/edit">-->
+      <!--          <IssueEditPage />-->
+      <!--        </Route>-->
+      <!--        <Route exact path="/issues/new">-->
+      <!--          <IssueCreatePage />-->
+      <!--        </Route>-->
+      <Route exact path="/issues">
+        <!--        <IssueListPage />-->
+      </Route>
+      <!--      <Route exact path="/search">-->
+      <!--        <SearchResultPage />-->
+      <!--      </Route>-->
+      <!--      <Route exact path="/">-->
+      <!--        <Redirect to="/issues" />-->
+      <!--      </Route>-->
+      <!--      <Route path="*">-->
+      <!--        <NotFoundPage />-->
+      <!--      </Route>-->
+    </section>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          Powered by <a href="https://github.com/ubermanu/tiquet" target="_blank">Tiquet</a>
+        </p>
+      </div>
+    </footer>
+  </main>
+</Router>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.min.css'
 import App from './App.svelte'
 
 const app = new App({
