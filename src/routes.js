9e42bcb5650e3bae2cd7d1df470bcf270b24795f
ubermanu <e.vodor@gmail.com>
2020-12-17 17:31:58 +0000

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import IssueCreatePage from './pages/IssueCreatePage.svelte'
+import NotFoundPage from './pages/NotFoundPage.svelte'
 
 export default {
-  '/issues/new': IssueCreatePage
+  '/issues/new': IssueCreatePage,
+  '*': NotFoundPage
 }
