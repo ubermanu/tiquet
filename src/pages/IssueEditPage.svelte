339c36ef2f19370d31d61db33a5b42e74c6a1ac8
ubermanu <e.vodor@gmail.com>
2020-12-17 18:28:21 +0000

    Add edit page

---
+++ src/pages/IssueEditPage.svelte
@@ -0,0 +1,45 @@
+<script>
+  import NotFoundPage from './NotFoundPage.svelte'
+  import issueStore from '../stores/issueStore'
+
+  export let params = {}
+  $: issue = issueStore.find(params.id)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    issueStore.save(issue)
+    // history.push(`#/issue/${issue.id}`)
+  }
+</script>
+
+{#if issue}
+  <div class="container">
+    <h1 class="title is-1">Edit an issue</h1>
+    <form on:submit={handleSubmit}>
+      <div class="field">
+        <label class="label">Title</label>
+        <div class="control">
+          <input name="title" class="input" type="text" required bind:value={issue.title} />
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Description</label>
+        <div class="control">
+          <textarea name="description" class="textarea" bind:value={issue.description}></textarea>
+        </div>
+      </div>
+
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link">Save</button>
+        </div>
+        <div class="control">
+          <a href="#/issue/{issue.id}" class="button is-link is-light">Cancel</a>
+        </div>
+      </div>
+    </form>
+  </div>
+{:else}
+  <NotFoundPage />
+{/if}
--- src/stores/issueStore.js
+++ src/stores/issueStore.js
@@ -1,4 +1,5 @@
 import { writable, get } from 'svelte/store'
+import { exclude } from '../reducers/filters'
 
 export const issueStore = writable([])
 const { subscribe, set, update } = issueStore
@@ -7,5 +8,6 @@ export default {
   subscribe,
   add: (issue) => update(issues => [...issues, issue]),
   find: (issueId) => get(issueStore).filter(({ id }) => id === issueId)[0],
+  save: (issue) => update(issues => [...issues.filter(exclude(issue.id)), issue]),
   reset: () => set([])
 }
