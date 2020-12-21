d8533dcb1c3796d83f3aa62aff54f9b06d02d075
ubermanu <e.vodor@gmail.com>
2020-12-21 16:17:38 +0000

    Add edit page

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import IssueEditPage from './pages/IssueEditPage.svelte'
 import IssueCreatePage from './pages/IssueCreatePage.svelte'
 import NotFoundPage from './pages/NotFoundPage.svelte'
 import IssueDetailPage from './pages/IssueDetailPage.svelte'
@@ -5,6 +6,7 @@ import IssueListPage from './pages/IssueListPage.svelte'
 
 export default {
   '/issues/new': IssueCreatePage,
+  '/issues/:id/edit': IssueEditPage,
   '/issues/:id': IssueDetailPage,
   '/issues': IssueListPage,
   '/': IssueListPage,
