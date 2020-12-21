6831343dddff4187640738230040e5de79abec29
ubermanu <e.vodor@gmail.com>
2020-12-21 14:39:53 +0000

    Delete redux data

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import issueReducer from './issueReducer'
-import messageReducer from './messageReducer'
-
-export default combineReducers({
-  issues: issueReducer,
-  messages: messageReducer
-})
