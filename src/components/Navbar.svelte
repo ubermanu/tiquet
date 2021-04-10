9bf09df8625ec1ca6ab620c011fdff95b5d946ec
ubermanu <e.vodor@gmail.com>
2021-04-10 12:34:40 +0200

    update create issue page and nav

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,42 +1,11 @@
 <script>
+  import { ButtonSet, Button } from 'carbon-components-svelte'
   import SearchForm from './NavbarSearchForm.svelte'
 
   let opened = false
 </script>
 
-<nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <div class="navbar-item">
-      <span class="is-size-4">🦟</span>
-    </div>
-    <a role="button" aria-label="menu" aria-expanded="false"
-       class="navbar-burger burger" class:is-active={opened} on:click={() => opened = !opened}>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-  <div class="navbar-menu" class:is-active={opened}>
-    <div class="navbar-start">
-      <a href="#/issues/new" class="navbar-item">
-        <span class="icon">
-          <i class="fa fa-plus"></i>
-        </span>
-        {' '}
-        <span>New issue</span>
-      </a>
-      <a href="#/issues" class="navbar-item">
-        <span class="icon">
-          <i class="fa fa-list"></i>
-        </span>
-        {' '}
-        <span>Issues</span>
-      </a>
-    </div>
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <SearchForm />
-      </div>
-    </div>
-  </div>
-</nav>
+<ButtonSet role="navigation" aria-label="main navigation">
+  <Button href="#/issues/new" kind="secondary">New issue</Button>
+  <Button href="#/issues" kind="secondary">Issues</Button>
+</ButtonSet>
--- src/pages/IssueCreatePage.svelte
+++ src/pages/IssueCreatePage.svelte
@@ -1,4 +1,11 @@
 <script>
+  import {
+    Form,
+    ButtonSet,
+    Button,
+    TextInput,
+    TextArea
+  } from 'carbon-components-svelte'
   import { writable } from 'svelte/store'
   import { addIssue } from '../stores/issues'
 
@@ -13,49 +20,26 @@
   }
 </script>
 
-<div class="container">
-  <h1 class="title is-1">New issue</h1>
-  <div class="columns">
-    <div class="column is-half">
-      <form on:submit|preventDefault={handleSubmit}>
-        <div class="field">
-          <label class="label" for="issue-title">Title</label>
-          <div class="control">
-            <input id="issue-title" name="title" class="input" type="text" bind:value={$issue.title} required />
-          </div>
-        </div>
+<h1>New issue</h1>
+<Form on:submit>
+  <TextInput
+    labelText="Title"
+    name="title"
+    class="input"
+    type="text"
+    bind:value={$issue.title}
+    required
+  />
 
-        <div class="field">
-          <label class="label" for="issue-desc">Description</label>
-          <div class="control">
-            <textarea id="issue-desc" name="description" class="textarea" bind:value={$issue.description}></textarea>
-          </div>
-        </div>
+  <TextArea
+    labelText="Description"
+    name="description"
+    class="textarea"
+    bind:value={$issue.description}
+  />
 
-        <div class="field is-grouped">
-          <div class="control">
-            <button class="button is-link">Submit</button>
-          </div>
-          <div class="control">
-            <a href="#/issues" class="button is-link is-light">Cancel</a>
-          </div>
-        </div>
-      </form>
-    </div>
-    <div class="column is-half">
-      <div class="label">Matching issues</div>
-      <article class="message is-small">
-        <div class="message-body">
-          Check if the issue does not exist already
-        </div>
-      </article>
-      <!--{alikeIssues.map(issue => (-->
-      <!--  <div class="card mb-4" key={issue.id}>-->
-      <!--    <div class="card-content">-->
-      <!--      <span class="is-size-6">{issue.title}</span>-->
-      <!--    </div>-->
-      <!--  </div>-->
-      <!--))}-->
-    </div>
-  </div>
-</div>
+  <ButtonSet>
+    <Button>Submit</Button>
+    <Button kind="secondary">Cancel</Button>
+  </ButtonSet>
+</Form>
