99558eeb79e3227674178b539ddcc6329963ceea
ubermanu <e.vodor@gmail.com>
2021-04-16 17:18:50 +0200

    Remove query-string from search form

--- src/components/NavbarSearchForm.svelte
+++ src/components/NavbarSearchForm.svelte
@@ -1,14 +1,12 @@
 <script>
   import { writable } from 'svelte/store'
-  import queryString from 'query-string'
 
-  let formData = writable({ q: '' })
+  let search = writable('')
 
   // TODO: Fix search params
   function handleSubmit(e) {
     e.preventDefault()
-    const query = queryString.stringify($formData)
-    history.push(e.target.action + '?' + query)
+    history.push(e.target.action + '/' + encodeURIComponent($search))
   }
 </script>
 
@@ -16,7 +14,7 @@
   <div class="field">
     <div class="control has-icons-left has-icons-right">
       <input class="input" type="text" name="q" placeholder="Find an issue..."
-             aria-label="Search an issue using a keyword" required bind:value={$formData.q} />
+             aria-label="Search an issue using a keyword" required bind:value={$search} />
       <span class="icon is-small is-left">
         <i class="fas fa-search"></i>
       </span>
