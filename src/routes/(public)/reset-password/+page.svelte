61fe753c638f49191ab3fa141b444633459f8b8d
ubermanu <manu@paca.happy-dev.fr>
2023-04-25 14:35:24 +0200

    chore: cleanup reset pw

--- src/routes/(public)/reset-password/+page.svelte
+++ src/routes/(public)/reset-password/+page.svelte
@@ -1,27 +1,24 @@
 <script>
-  import { Icon, InformationCircle } from 'svelte-hero-icons';
-  import Input from '$components/Form/Input.svelte';
+  import { Icon, InformationCircle } from 'svelte-hero-icons'
+  import Input from '$components/Form/Input.svelte'
+  import Alert from '$components/Alert.svelte'
 
-  export let form;
+  export let form
 </script>
 
-<div class="container mx-auto px-6 my-10">
+<div class="mx-auto px-6 my-10">
   <form action="?/resetPassword" method="POST" class="max-w-xs mx-auto">
-    <fieldset>
+    <fieldset class="fieldset">
       <Input label="Email" name="email" type="email" required={true} />
-      <div class="form-control w-full mt-6">
-        <button type="submit" class="btn btn-primary">Request Password Reset</button>
-      </div>
+      <button type="submit" class="btn variant-filled">
+        Request Password Reset
+      </button>
+      {#if form?.success}
+        <Alert level="success">
+          A password reset link has been sent to your email address if it exists
+          in our system.
+        </Alert>
+      {/if}
     </fieldset>
   </form>
-  {#if form?.success}
-    <div class="alert alert-info shadow-lg mt-6 w-full max-w-md mx-auto">
-      <div>
-        <Icon src={InformationCircle} class="stroke-current flex-shrink-0 w-6 h-6" />
-        <span
-          >A password reset link has been sent to your email address if it exists in our system.</span
-        >
-      </div>
-    </div>
-  {/if}
 </div>
