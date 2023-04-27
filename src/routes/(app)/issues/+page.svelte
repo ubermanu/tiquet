352ff89a7ea5463f3a9f5575f23c056808f8b52b
ubermanu <manu@paca.happy-dev.fr>
2023-04-27 10:47:44 +0200

    feat: add actions to the collection table

--- src/routes/(app)/issues/+page.svelte
+++ src/routes/(app)/issues/+page.svelte
@@ -1,8 +1,9 @@
 <script>
-  import { Table, tableMapperValues } from '@skeletonlabs/skeleton'
+  import { Eye, Icon, Pencil, Plus, Trash } from 'svelte-hero-icons'
+  import { tableMapperValues } from '@skeletonlabs/skeleton'
   import { readableDate } from '$lib/helpers.js'
   import Pages from '$lib/components/Pages.svelte'
-  import { Icon, Plus } from 'svelte-hero-icons'
+  import confirm from '$lib/actions/confirm.js'
 
   export let data
 
@@ -21,11 +22,6 @@
     body: tableMapperValues(source, ['title', 'created', 'updated']),
     meta: tableMapperValues(source, ['id']),
   }
-
-  function handleSelect(event) {
-    const [id] = event.detail
-    window.location.href = `/issues/${id}`
-  }
 </script>
 
 <div class="mb-8 flex flex-wrap items-center">
@@ -36,7 +32,53 @@
   </a>
 </div>
 
-<Table source={tableSource} interactive={true} on:selected={handleSelect} />
+<div class="table-container ">
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        {#each tableSource.head as head}
+          <th>{head}</th>
+        {/each}
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each tableSource.body as row, i}
+        <tr>
+          {#each row as cell}
+            <td>{cell}</td>
+          {/each}
+          <td>
+            <div class="flex gap-2 items-center">
+              <a
+                class="btn variant-ghost btn-xs"
+                href={`/issues/${tableSource.meta[i][0]}`}
+                title="View"
+              >
+                <Icon src={Eye} class="w-4 h-4 mr-1" />
+              </a>
+              <a
+                class="btn variant-ghost btn-xs"
+                href={`/issues/${tableSource.meta[i][0]}/edit`}
+                title="Edit"
+              >
+                <Icon src={Pencil} class="w-4 h-4 mr-1" />
+              </a>
+              <a
+                class="btn variant-ghost-error btn-xs"
+                href={`/issues/${tableSource.meta[i][0]}/delete`}
+                title="Delete"
+                use:confirm={'Are you sure you want to delete this issue?'}
+              >
+                <Icon src={Trash} class="w-4 h-4 mr-1" />
+              </a>
+            </div>
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
 
 <br />
 
